d68075e50ac50b0c2db0690a3622743882524b6e
Turtle-1201 <christopherjamesmacapagong@gmail.com>
2025-03-05 16:50:47 +0800

    cors

---
+++ app/Http/Kernel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \Illuminate\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\CorsMiddleware::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Http\Middleware\ValidatePostSize::class,
+        // \Illuminate\Http\Middleware\TrimStrings::class,
+        // \Illuminate\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    protected $middlewareGroups = [
+        'web' => [
+            // \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            // \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,22 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::apiResource('posts', PostController::class);
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/show-user', [AuthController::class, 'show'])->middleware('auth:sanctum');
-Route::get('/show-user/{id}', [AuthController::class, 'showById'])->middleware('auth:sanctum');
-Route::put('/update-user', [AuthController::class, 'update'])->middleware('auth:sanctum');
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::middleware(['cors'])->group(function () {
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+    
+    Route::apiResource('posts', PostController::class);
+    
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/show-user', [AuthController::class, 'show'])->middleware('auth:sanctum');
+    Route::get('/show-user/{id}', [AuthController::class, 'showById'])->middleware('auth:sanctum');
+    Route::put('/update-user', [AuthController::class, 'update'])->middleware('auth:sanctum');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    
+})
 
 // Route::get('/post', function () {
 //     return 'API';
